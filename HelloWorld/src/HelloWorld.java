a4c17f46dff613fbbd8cc7ce6b3814f0bf151d55
simsim1536 <32252306+simsim1536@users.noreply.github.com>
2017-10-18 09:15:28 +0900

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,10 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Hello World hotfix version");
-		System.out.println("Hello World master XXXXXXX");
-		System.out.println("Hello World hotfix version");
-		System.out.println("Hello World hotfix XXXXXXX");
+		System.out.println("Hello World hotfix version\n");
+		System.out.println("Hello World master XXXXXXX\n");
+		System.out.println("Hello World hotfix version\n");
+		System.out.println("Hello World hotfix XXXXXXX\n");
+		
+		///////////////////////////////////////
+		System.out.println("hello github");
 	}
 
 }
